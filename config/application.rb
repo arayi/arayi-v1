a014c41597b5067b51584c57baea0fb031e09330
Melody Mustaine <mel.mustaine@gmail.com>
2014-02-11 01:03:47 -0800

    asset precompilation for heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Arayi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Make post-deploy asset precompilation happen
+    config.assets.initialize_on_precompile = true
   end
 end
