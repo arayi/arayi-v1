a02d3411df15e4176e424f300c03e82704a71d00
Melody Mustaine <meli@meliputer.kaidence.org>
2014-02-11 00:36:06 -0800

    I MADE A THING

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arayi::Application.config.secret_token = '5312b4d8fe3cb190eed0c12a2c84c5b26396f1edc116cab1dbb6f2af5299eb9fe987837ba8358e45266c26cce53a072fd6f10c32f92f07e37880c92ce20d96ed'
